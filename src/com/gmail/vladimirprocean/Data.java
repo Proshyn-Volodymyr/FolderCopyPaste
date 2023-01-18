37b5eff449c1c3fb947b29f4d01f7f1db3401241
Proshyn-Volodymyr <vladimirprocean@gmail.com>
2023-01-18 07:50:59 +0200

    Data saver class

---
+++ src/com/gmail/vladimirprocean/Data.java
@@ -0,0 +1,15 @@
+package com.gmail.vladimirprocean;
+
+public class Data {
+    private byte[] data;
+
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+}
